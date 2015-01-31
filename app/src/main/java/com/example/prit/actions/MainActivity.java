971ff25b207fe619f127353e41d69626bb510729
pmaipady <prithvirajm2@yahoo.com>
2015-01-30 17:18:51 -0800

    Initial commit

---
+++ app/src/main/java/com/example/prit/actions/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.prit.actions;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+    // TextView with context menu
+    private TextView tvName;
+    // Tracks current contextual action mode
+    private ActionMode currentActionMode;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvName = (TextView) findViewById(R.id.tvName);
+        tvName.setOnLongClickListener(new View.OnLongClickListener() {
+            // Called when the user long-clicks on someView
+            public boolean onLongClick(View view) {
+                if (currentActionMode != null) { return false; }
+                startActionMode(modeCallBack);
+                view.setSelected(true);
+                return true;
+            }
+        });
+    }
+    // Define the callback when ActionMode is activated
+    private ActionMode.Callback modeCallBack = new ActionMode.Callback() {
+        // Called when the action mode is created; startActionMode() was called
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            mode.setTitle("Actions");
+            mode.getMenuInflater().inflate(R.menu.actions_textview, menu);
+            return true;
+        }
+
+        // Called each time the action mode is shown.
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false; // Return false if nothing is done
+        }
+
+        // Called when the user selects a contextual menu item
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.menu_edit:
+                    Toast.makeText(MainActivity.this, "Editing!", Toast.LENGTH_SHORT).show();
+                    mode.finish(); // Action picked, so close the contextual menu
+                    return true;
+                case R.id.menu_delete:
+                    // Trigger the deletion here
+                    mode.finish(); // Action picked, so close the contextual menu
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        // Called when the user exits the action mode
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            currentActionMode = null; // Clear current action mode
+        }
+    };
+/*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
